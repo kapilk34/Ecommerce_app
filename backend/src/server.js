f17717ef198e7a13b2451b167d5fc5419c798817
kapilk34 <kapilkumargupta1511@gmail.com>
2026-01-07 20:23:32 +0530

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from "express";
+import path from "path";
+import { ENV } from "../src/config/env.js";
 
 const app = express();
+const _dirname = path.resolve()
 
 app.get("/api/health", (req, res) =>{
     res.status(200).json({message:"success"});
 });
 
-app.listen(3000, ()=>{
+//make our app ready for development
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname, "../admin/dist")))
+
+    app.get("/{*any}", (req, res) =>{
+        res.sendFile(path.json(_dirname, "../admin","dist","index.html"));
+    });
+}
+
+app.listen(ENV.PORT, ()=>{
     console.log("Server is running Successfully!")
 });
