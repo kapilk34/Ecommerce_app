05b95bde5c42b61adc27a9bb405fc2bd63db9aff
kapilk34 <kapilkumargupta1511@gmail.com>
2026-01-15 23:09:49 +0530

    updated admin routes

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -6,9 +6,32 @@ export const protectRoute = [
     requireAuth(),
     async (req,res,next) =>{
         try {
-            const clerkId = req.
+            const clerkId = req.auth().userId
+            if(!clerkId)
+                return res.status(401).json({message:"Unauthorized User"})
+
+            const user = await User.findOne({clerkId})
+            if(!User)
+                return res.status(404).json({message:"User not found!"})
+
+            req.user = user
+
+            next();
         } catch (error) {
-            
+            console.error("Enter in protectedRoute middleware", error)
+            res.status(500).json({message:"Internal server Error!"})
         }
     }
-]
+];
+
+export const adminOnly = (req,res,next) =>{
+    if(!req.user){
+        return res.status(401).json({message:"Unauthorised User"})
+    }
+    
+    if(req.user.email !== ENV.ADMIN_EMAIL){
+        return res.status(403).json({message:"Forbidder - admin access only"})
+    }
+
+    next();
+}
--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { createProduct } from "../controllers/adminController.js";
+import { adminOnly, protectRoute } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.post("/products", protectRoute, adminOnly, createProduct);
+router.use( protectRoute, adminOnly);
+
+router.post("/products", createProduct);
+router.get("/products", getAllProducts);
+router.put("/products/:id", getAllProducts);
 
 export default router;
