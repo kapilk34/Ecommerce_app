b80cfc9642776e87d1917b49c02e12a7fed0e98f
kapilk34 <kapilkumargupta1511@gmail.com>
2026-01-16 22:34:20 +0530

    product controllers

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -43,6 +43,48 @@ export async function createProduct(req,res) {
     }
 }
 
-export async function getAllProducts(req,res) {}
+export async function getAllProducts(_,res) {
+    try {
+        const products = (await Product.find()).toSorted({createdAt:-1})
+        res.status(200).json(products)
+    } catch (error) {
+        console.error("Error fetching products: ", error);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
 
-export async function updateProduct(req,res) {}
+export async function updateProduct(req,res) {
+    try {
+        const {id} = req.params
+        const { name, description, price, stock, category } = req.body;
+
+        const product = await Product.findById(id);
+        if(!product){
+            return res.status(404).json({message:"Product not found"});
+        }
+
+        if(name) product.name = name;
+        if(description) product.description = description;
+        if(price) product.price = price;
+        if(stock !== undefined) product.stock = parseInt(stock);
+        if(category) product.category = category;
+
+        if(req.files && req.files.length > 0){
+            if(req.files.length > 3) {
+                return res.status(400).json({message:"Maximum 3 images allowed"});
+            }
+
+            const uploadPromises = req.files.map((file) =>{
+                return cloudinary.uploader.upload(file.path, {
+                    folder:"products",
+                });
+            });
+
+            const uploadResults = await Promise.all(uploadPromises);
+            product.images = uploadResults.map
+        }
+    } catch (error) {
+        console.error("Error updating products: ", error);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
--- backend/src/middleware/multerMiddleware.js
+++ backend/src/middleware/multerMiddleware.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import path from "path";
 
-const storage = multer.diskStrorage({
+const storage = multer.diskStorage({
     filename: (req,file,cb)=>{
         cb(null, `${Date.now()}-${file.originalname}`)
     }
